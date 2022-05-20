8599d3c2a8358977c280ade5c755a603fc132358
Pabllot07 <pabllot07@outlook.com>
2022-05-20 01:02:59 -0300

    end of first day

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,19 +1,43 @@
 import React from "react";
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import { Bell } from 'react-feather'
+
+const useStyles = makeStyles({
+    appBar: {
+        boxShadow: 'none',
+    },
+    img: {
+        maxHeight: 55,
+    }
+})
 
 function Header(){
+const classes = useStyles();
+
     return (
-        <header className="header">
-             <div className="toolbar">
-                 <div className="">
+        <AppBar position="fixed" color="inherit" className={classes.appBar}>
+             <Toolbar>
+                 <img src='/images/logo.png' alt="logo" className={classes.img}/>
+                 <div>
+                    <Button color='primary' variant='contained'>Novo Post</Button>
+                    <SvgIcon>
+                        <Bell />
+                    </SvgIcon>
+                 </div>
+                 {/* <div className="">
                      <span>Conecta Dev</span>
                  </div>
                  <div className="">
-                     <button>Novo Post</button>
+                     
                      <span>img1</span>
                      <span>img2</span>
-                 </div>
-             </div>
-           </header>
+                 </div> */}
+             </Toolbar>
+        </AppBar>
     )
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,36 @@
 import React from "react";
-import "./style.css"
+// import "./style.css"
+import { makeStyles } from '@material-ui/core/styles'
+
 import Header from "./components/Header";
+import Feed from "./Feed";
+import Navbar from "./Navbar";
+
+
+const useStyles = makeStyles({
+    root: {
+       display: 'flex',
+       flexDirection: 'column', 
+    },
+    main: {
+        height: '100vh',
+        display: 'flex',
+        width: '1200px',
+        margin: '0 auto',
+    }
+})
+
 
 function Home(){
+    const classes = useStyles();
+
    return (
-       <div>
+       <div className={classes.root}>
            <Header />
-            <main className="main">
-               <div className="navbar">navbar</div>
-               <div className="feed">feed</div>
+           <div className="toolbar"></div>
+            <main className={classes.main}>
+               <Navbar />
+               <Feed />
            </main>
        </div>
    )
