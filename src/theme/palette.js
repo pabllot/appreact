593df2259cba61aff7929daea25b48477b9ee523
Pabllot07 <pabllot07@outlook.com>
2022-05-21 15:38:26 -0300

    customization done

---
+++ src/theme/palette.js
@@ -0,0 +1,15 @@
+import { colors } from '@material-ui/core'
+
+export default {
+    
+        primary: {
+            dark: colors.red[400],
+            main: colors.red[900],
+            light: colors.red['A700'],
+        },
+        secondary: {
+            dark: colors.orange['A700'],
+            main: colors.orange['A700'],
+            light: colors.orange['A200'],
+        },
+}
