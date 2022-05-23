64fb3f23300eeb9515472dfa8335033fde7bf34e
Pabllot07 <pabllot07@outlook.com>
2022-05-23 16:54:22 -0300

    React Router implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Home from "./pages/Home";
 import { ThemeProvider } from '@material-ui/core/styles';
-
+import SignIn from "./pages/SignIn";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import theme from './theme';
 
 function App() {
+
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="*" element={<h1>Not Found 404! DickHead</h1>} />
+        </Routes>      
+      </BrowserRouter>
+      {/* {
+        url === 'http://localhost:3000/'
+        ? <Home />
+        : <SignIn />
+      } */}
     </ThemeProvider>
     
   );
